c551b0485d2410ec27a9c312ca78f29e9bc78e59
juanbenedhit <87563952+juanbenedhit@users.noreply.github.com>
2024-11-07 22:08:19 +0700

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Image src="/lisk.png" alt="Lisk" width={400} height={400} />
+    </main>
+  );
+}
